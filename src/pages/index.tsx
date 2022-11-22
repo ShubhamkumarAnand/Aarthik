5e40a59d0cd300a68c12a23210792ce9743c41fe
imskanand <the.anand3099@gmail.com>
2022-11-22 23:07:26 +0530

    Again Scaffold the App using the T3-Stack

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Aarthik</title>
+        <meta name="description" content="Aarthik Bank : Modern Bank Account" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Hello Aarthik Bank</main>
+    </>
+  );
+};
+
+export default Home;
